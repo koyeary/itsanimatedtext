e3f46eda1337de020aec42cef10c65b8d5ae3814
koyeary <katyeary@gmail.com>
2020-11-12 13:58:27 -0800

    get all products, get one product, update product

--- routes/api/shop.js
+++ routes/api/shop.js
@@ -1,10 +1,24 @@
 const express = require('express');
 const router = express.Router();
 const auth = require('../../middleware/auth');
-/* const { check, 
-  validationResult } = require('express-validator'); */
+const { check, validationResult } = require('express-validator');
 
 const Product = require('../../models/Product');
+
+
+// @route    GET api/shop/
+// @desc     Get all products
+// @access   Public
+router.get('/', async (req, res) => {
+  try {
+    const products = await Product.find();
+    res.json(products);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+});
+
 
 // @route    GET api/shop/admin
 // @desc     Get product in this shop
@@ -14,9 +28,9 @@ router.get('/admin', auth, async (req, res) => {
     const product = await Product.findOne({ _id: req.body.id });
 
     if (!product) {
-      return res
-        .status(400)
-        .json({ msg: 'Product not found: there is no product matching this id' });
+      return res.status(400).json({
+        msg: 'Product not found: there is no product matching this id'
+      });
     }
 
     res.json(product);
@@ -29,31 +43,86 @@ router.get('/admin', auth, async (req, res) => {
 // @route    POST api/shop
 // @desc     Create a product
 // @access   Private
-router.post('/admin', auth, async (req, res) => {
-  try {
-    const newProduct = new Product({
-      name: req.body.name,
-      image_src: req.body.image_src,
-      price: req.body.price,
-      description: req.body.description
-    });
+router.post(
+  '/admin',
+  [
+    auth,
+    [
+      check('name', 'Name is required'),
+      check('image_src', 'Image source is required'),
+      check('price', 'Price is required')
+        .not()
+        .isEmpty()
+        .custom((value, { req }) => (req.body.to ? value < req.body.to : true))
+    ]
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
 
-    const product = await newProduct.save();
+    try {
+      const newProduct = new Product({
+        name: req.body.name,
+        image_src: req.body.image_src,
+        price: req.body.price,
+        description: req.body.description
+      });
 
-    res.json(product);
-  } catch (err) {
-    console.error(err.message);
-    res.status(500).send('Server Error');
+      const product = await newProduct.save();
+
+      res.json(product);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server Error');
+    }
   }
-});
+);
 
-/* router.put('/admin', auth, async (req, res) => {
+// @route    PUT api/shop
+// @desc     Update a product
+// @access   Private
+router.put(
+  '/admin',
+  [
+    auth,
+    [
+      check('name', 'Name is required'),
+      check('image_src', 'Image source is required'),
+      check('price', 'Price is required')
+        .not()
+        .isEmpty()
+        .custom((value, { req }) => (req.body.to ? value < req.body.to : true))
+    ]
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
 
-}) */
+    const { name, image_src, price, description } = req.body;
 
-// @route    GET api/shop/
-// @desc     Get products in this shop
-// @access   Public
-//router.get('/', (req, res) => res.send('shop route'));
+    try {
+      const product = await Product.findOne({ _id: req.body.id });
+      if (!product) {
+        return res.status(400).json({
+          msg: 'Product not found: there is no product matching this id'
+        });
+      }
+      (product.name = name),
+        (product.image_src = image_src),
+        (product.price = price),
+        (product.description = description);
+
+      await product.save();
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server Error');
+    }
+  }
+);
+
 
 module.exports = router;

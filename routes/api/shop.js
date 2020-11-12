1fb22eb89f363540f6a9595b9ced18dfcc7ffdc9
koyeary <katyeary@gmail.com>
2020-11-12 13:40:54 -0800

    validate required product fields in api

--- routes/api/shop.js
+++ routes/api/shop.js
@@ -1,53 +1,55 @@
 const express = require('express');
 const router = express.Router();
-//const auth = require('.../../middleware/auth');
+const auth = require('../../middleware/auth');
+/* const { check, 
+  validationResult } = require('express-validator'); */
 
 const Product = require('../../models/Product');
-
 
 // @route    GET api/shop/admin
 // @desc     Get product in this shop
 // @access   Private
-router.get('/admin', async (req, res) => {
+router.get('/admin', auth, async (req, res) => {
   try {
-    const product = await Product.findOne({ _id: req.body.id});
+    const product = await Product.findOne({ _id: req.body.id });
 
-    if(!product) {
-      return res.status(400).json({ msg: 'There is no product matching this ID'});
+    if (!product) {
+      return res
+        .status(400)
+        .json({ msg: 'Product not found: there is no product matching this id' });
     }
 
     res.json(product);
   } catch (err) {
     console.error(err.message);
-    res.status(500).send('Server Error')
+    res.status(500).send('Server Error');
   }
 });
 
 // @route    POST api/shop
 // @desc     Create a product
 // @access   Private
-router.post(
-  '/',
-  async (req, res) => {
+router.post('/admin', auth, async (req, res) => {
+  try {
+    const newProduct = new Product({
+      name: req.body.name,
+      image_src: req.body.image_src,
+      price: req.body.price,
+      description: req.body.description
+    });
 
-    try {
+    const product = await newProduct.save();
 
-      const newProduct = new Product({
-        name: req.body.name,
-        image_src: req.body.image_src,
-        price: req.body.price,
-        description: req.body.description
-      });
-
-      const product = await newProduct.save();
-
-      res.json(product);
-    } catch (err) {
-      console.error(err.message);
-      res.status(500).send('Server Error');
-    }
+    res.json(product);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
   }
-);
+});
+
+/* router.put('/admin', auth, async (req, res) => {
+
+}) */
 
 // @route    GET api/shop/
 // @desc     Get products in this shop

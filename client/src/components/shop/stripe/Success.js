e3e12861f567967af7c40ce0e70f042a385339a3
koyeary <katyeary@gmail.com>
2020-12-05 15:24:24 -0800

    add success window, checkout session registers in stripe account.

---
+++ client/src/components/shop/stripe/Success.js
@@ -0,0 +1,12 @@
+import React, { Fragment } from 'react';
+
+const Success = () => {
+  <Fragment>
+    <h1>Thanks for your order!</h1>
+    <p>
+      We appreciate your business! If you have any questions, please email orders@example.com.
+    </p>
+  </Fragment>;
+};
+
+export default Success;
